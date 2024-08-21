28cfea3e8e32ba45df6df5e572505866727e222f
A-G-D24 <angelonogueira.aluno@unipampa.edu.br>
2024-08-20 22:20:42 -0300

    Add files via upload

--- Algoritmos_de_Teste/test_dataset.py
+++ Algoritmos_de_Teste/test_dataset.py
@@ -10,6 +10,15 @@ from termcolor import colored, cprint
 #from ydata_quality import DataQuality
 
 def parse_args(argv):
+    """
+    Analisa os argumentos da linha de comando para a execução do script.
+
+    Parâmetros:
+    - argv : Lista de strings representando os argumentos da linha de comando. Normalmente, isso é passado automaticamente pelo sistema quando o script é executado.
+
+    Retorna:
+    - args: Um objeto Namespace contendo os argumentos analisados e seus valores.
+    """
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument(
         '-d', '--dataset', metavar = 'DATASET',
@@ -30,6 +39,17 @@ def print_info(message):
     print(colored(message, 'green'))
 
 def get_X_y(args, dataset):
+    """
+    Extrai as características (X) e o rótulo de classe (y) do conjunto de dados com base na coluna de classe fornecida.
+
+    Parâmetros:
+    - args : Objeto contendo os argumentos analisados, incluindo o nome da coluna de classe.
+    - dataset : Conjunto de dados carregado em um DataFrame do pandas.
+
+    Retorna:
+    - X : DataFrame contendo todas as colunas do conjunto de dados, exceto a coluna de classe.
+    - y :Série contendo os valores da coluna de classe do conjunto de dados.
+    """
     if(args.class_column not in dataset.columns):
         print(f'Class Column {args.class_column} Not Found in Dataset {args.dataset}')
         return None, None
@@ -38,6 +58,18 @@ def get_X_y(args, dataset):
     return X, y
 
 def calculate_mig_features(features, target):
+    """
+    Calcula o ganho de informação mútua (Mutual Information Gain) para as características fornecidas e retorna um DataFrame contendo a importância de cada característica.
+
+    Parâmetros:
+    - features : DataFrame contendo as características (features) do conjunto de dados, onde cada coluna representa uma característica.
+    - target : Série contendo os rótulos de classe ou variáveis alvo associadas às amostras em `features`.
+
+    Retorna:
+    - df: DataFrame contendo a importância das características calculada pelo ganho de informação mútua. O DataFrame possui duas colunas:
+        - "feature": Nome das características.
+        - "score": Valor do ganho de informação mútua associado a cada característica.
+    """   
     features_names = features.columns
     mutualInformationGain = mutual_info_classif(features, target, random_state = 0)
     data = {'feature': features_names, 'score': mutualInformationGain}
@@ -46,6 +78,13 @@ def calculate_mig_features(features, target):
     return df
 
 def mig_features(dataset, args):
+    """
+    Avalia  o ganho de Informação Mútua (Mutual Information Gain) das características de um conjunto de dados e exibe a distribuição dessas pontuações.
+
+    Parâmetros:
+    - dataset : DataFrame contendo o conjunto de dados que inclui as características e a coluna de classe.
+    - args : Objeto contendo os argumentos de entrada que definem parâmetros como a coluna de classe e outros parâmetros necessários para a função `get_X_y`.
+    """
     print_info('>>> Evaluating Information Gain <<<')
     X_dataset, y_dataset = get_X_y(args, dataset)
     mig = calculate_mig_features(X_dataset, y_dataset)
@@ -54,6 +93,13 @@ def mig_features(dataset, args):
     print_distribution(mig, 'score', 0.1, max_ig)
 
 def duplicate_samples(dataset, args):
+    """
+    Avalia a quantidade de amostras duplicadas em um conjunto de dados e calcula a porcentagem de redução ao remover duplicatas.
+
+    Parâmetros:
+    - dataset : DataFrame contendo o conjunto de dados a ser analisado, incluindo as características.
+    - args : Objeto contendo os argumentos de entrada que definem parâmetros como a coluna de classe e se a coluna de classe deve ser descartada.
+    """
     print_info('>>> Evaluating Duplicate Samples <<<')
     ds = dataset
     if args.drop_class:
@@ -69,6 +115,13 @@ def duplicate_samples(dataset, args):
     print(f'Reduction >> {percent:.2f}%')
 
 def frequency_features(dataset, args):
+    """
+    Avalia a frequência das características em um conjunto de dados e calcula a porcentagem máxima de ocorrência.
+
+    Parâmetros:
+    - dataset : DataFrame contendo o conjunto de dados a ser analisado, incluindo as características e, possivelmente, a coluna de classe.
+    - args : Objeto contendo os argumentos de entrada que definem parâmetros como a coluna de classe.
+    """
     print_info('>>> Evaluating Frequency of Features <<<')
     ft_sum = dataset.sum()
     ft_sum.drop(labels = [args.class_column], inplace = True)
@@ -79,6 +132,19 @@ def frequency_features(dataset, args):
     print_distribution(fdf, 'frequency', 0.1, len(dataset))
 
 def calculate_rf_features_importance(features, target):
+    """
+    Calcula a importância das características utilizando um classificador Random Forest e retorna um DataFrame com as características mais importantes.
+
+    Parâmetros:
+    - features :  DataFrame contendo as características (features) do conjunto de dados, onde cada coluna representa uma característica.
+    - target : Série contendo os rótulos de classe ou variáveis alvo associadas às amostras em `features`.
+
+    Retorna:
+    - df : DataFrame
+        DataFrame contendo a importância das características calculada pelo classificador Random Forest. O DataFrame possui duas colunas:
+        - "feature": Nome das características.
+        - "importance": Importância associada a cada característica.
+    """
     features_names = features.columns
     model = RandomForestClassifier(random_state = 0)
     model.fit(features, target)
@@ -90,6 +156,13 @@ def calculate_rf_features_importance(features, target):
     return df
 
 def rf_features_importance(dataset, args):
+    """
+    Avalia a importância das características usando um classificador Random Forest e imprime a importância máxima e a distribuição das importâncias das características.
+
+    Parâmetros:
+    - dataset :DataFrame contendo o conjunto de dados. A última coluna é assumida como a coluna de classe (rótulos), enquanto as demais são características.
+    - args : Objeto contendo os argumentos de linha de comando, incluindo o nome da coluna de classe e uma opção para verbosidade.
+    """
     print_info('>>> Evaluating RF Features Importance <<<')
     X_dataset, y_dataset = get_X_y(args, dataset)
     rffi = calculate_rf_features_importance(X_dataset, y_dataset)
@@ -99,6 +172,13 @@ def rf_features_importance(dataset, args):
         print_distribution(rffi, 'importance', 0.1)
 
 def correlation_coefficient(dataset, args):
+    """
+    Avalia o coeficiente de correlação entre características no conjunto de dados e imprime informações sobre pares de características com alta e baixa correlação.
+
+    Parâmetros:
+    - dataset :DataFrame contendo o conjunto de dados. A última coluna é assumida como a coluna de classe (rótulos), enquanto as demais são características.
+    - args : Objeto contendo os argumentos de linha de comando, incluindo o nome da coluna de classe e uma opção para verbosidade.
+    """
     print_info('>>> Evaluating Correlation Coefficient <<<')
     X_dataset, y_dataset = get_X_y(args, dataset)
     correlation = X_dataset.corr(method = 'kendall')
@@ -130,6 +210,13 @@ def correlation_coefficient(dataset, args):
         print(df)
 
 def vif(dataset, args):
+    """
+    Avalia o Fator de Inflação da Variância (VIF) para cada característica no conjunto de dados e imprime informações sobre características com alta colinearidade.
+
+    Parâmetros:
+    - dataset : DataFrame contendo o conjunto de dados. A última coluna é assumida como a coluna de classe (rótulos), enquanto as demais são características.
+    - args : Objeto contendo os argumentos de linha de comando, incluindo o nome da coluna de classe e uma opção para verbosidade.
+    """
     from statsmodels.stats.outliers_influence import variance_inflation_factor
     print_info('>>> Evaluating Variance Inflation Factor (VIF) <<<')
     X_dataset, y_dataset = get_X_y(args, dataset)
@@ -146,6 +233,19 @@ def vif(dataset, args):
         print(high_vif)
 
 def calculate_permutation_features_importance(features, target):
+    """
+    Calcula a importância das características utilizando a técnica de importância por permutação e retorna as características mais importantes com base na importância calculada.
+
+    Parâmetros:
+    - features :DataFrame contendo as características do conjunto de dados, onde cada coluna representa uma característica.
+    - target : Série contendo os rótulos de classe ou variáveis alvo associadas às amostras em `features`.
+
+    Retorna:
+    - df : DataFrame
+        DataFrame contendo as características ordenadas pela importância calculada por permutação. O DataFrame possui duas colunas:
+            - "feature": Nome das características.
+            - "importance": Importância calculada para cada característica.
+    """
     from sklearn.inspection import permutation_importance
     features_names = features.columns
     model = RandomForestClassifier(random_state = 0)
@@ -159,6 +259,13 @@ def calculate_permutation_features_importance(features, target):
     return df
 
 def permutation_features_importance(dataset, args):
+    """
+    Avalia a importância das características utilizando a técnica de importância por permutação e exibe os resultados.
+
+    Parâmetros:
+    - dataset : DataFrame contendo o conjunto de dados completo, incluindo características e coluna de classe.
+    - args :  Objeto contendo argumentos e opções, incluindo o nome da coluna de classe e se a saída detalhada deve ser exibida.
+    """
     print_info('>>> Evaluating Permutation Features Importance <<<')
     X_dataset, y_dataset = get_X_y(args, dataset)
     pfi = calculate_permutation_features_importance(X_dataset, y_dataset)
@@ -168,6 +275,15 @@ def permutation_features_importance(dataset, args):
         print_distribution(pfi, 'importance', 0.1)
 
 def print_distribution(df, metric, step, max_value = 1.0):
+    """
+    Imprime a distribuição das características com base em um critério métrico em diferentes níveis de limiar.
+
+    Parâmetros:
+    - df :  DataFrame contendo as características e suas respectivas métricas. Deve ter pelo menos duas colunas: uma para a métrica e uma para o nome da característica.
+    - metric : Nome da coluna no DataFrame que contém os valores das métricas para as características.
+    - step : O incremento do limiar para a análise da distribuição. Por exemplo, um passo de 0.1 resulta em limiares de 0.0, 0.1, 0.2, ..., 1.0.
+    - max_value : O valor máximo da métrica utilizado para calcular os limiares. Valor padrão é 1.0;
+    """
     pd.options.display.float_format = '{:.2f}'.format
     df.style.hide_index()
     count = 0
